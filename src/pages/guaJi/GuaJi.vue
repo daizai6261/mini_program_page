54fdb8b2860720d582838d31fc71e94482551687
1354009873@qq.com <1354009873@qq.com>
2025-02-27 19:38:14 +0800

    first commit

---
+++ src/pages/guaJi/GuaJi.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+      Guaji
+    </div>
+  </template>
---
+++ src/router/route.js
@@ -0,0 +1,32 @@
+
+import VueRouter from 'vue-router'
+import GuaJi from '@/pages/guaJi/GuaJi.vue';
+import HomePage from '@/pages/index/HomePage.vue';
+import Vue from 'vue';
+
+Vue.use(VueRouter);
+const routes = [
+    // 以 / 开头的嵌套路径会被当作根路径
+    {
+        path: "/home",
+        component: HomePage,
+        name: "HomePage",
+        // children: [
+        //     { path: "guaJi", component: GuaJi, name: "GuaJi" }
+        // ]
+    },
+    {
+        path: "/guaJi",
+        component: GuaJi,
+        name: "GuaJi",
+        // children: [
+        //     { path: "guaJi", component: GuaJi, name: "GuaJi" }
+        // ]
+    },
+    { path: "*", redirect: { name: "HomePage" } }
+];
+const router = new VueRouter({
+    routes // （缩写）相当于 routes: routes
+});
+
+export default router;
